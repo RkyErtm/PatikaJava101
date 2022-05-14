0d858ed8c8d8a75deb6e4269524f62b0cc3d2570
RkyErtm <rkyertm1209@gmail.com>
2022-05-14 16:25:57 +0300

    updated

--- src/Giris/VucutKitleIndexi.java
+++ src/Giris/VucutKitleIndexi.java
@@ -1,2 +1,24 @@
-package Giris;public class VucutKitleIndexi {
+package Giris;
+
+import java.util.Scanner;
+
+public class VucutKitleIndexi {
+    /*
+        Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Aşağıdaki formüle göre kullanıcının
+        "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+        Formül
+        Kilo (kg) / Boy(m) * Boy(m)
+         */
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Boyunuzu giriniz: ");
+        double boy=inp.nextDouble();
+        System.out.print("Kilonuzu giriniz: ");
+        int kilo=inp.nextInt();
+        double index=kilo/(boy*boy);
+
+        System.out.println("boy:"+boy);
+        System.out.println("kilo:"+kilo);
+        System.out.println("Vücut kitle indexiniz: "+index);
+    }
 }
